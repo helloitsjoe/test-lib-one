73db451a802813d533f250f27b3869108ee02a40
helloitsjoe <jboyle617@gmail.com>
2020-11-09 13:22:39 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const Button = styled.button`
-//   background: none;
-//   border: none;
-//   border-radius: 4px;
-//   padding: 10px;
-//   background-color: cornflowerblue;
-//   color: white;
-// `;
+const Button = styled.button`
+  background: none;
+  border: none;
+  border-radius: 4px;
+  padding: 10px;
+  background-color: cornflowerblue;
+  color: white;
+`;
 
-// export const HomebaseButton = () => <Button>HOMEBASE</Button>;
-export const HomebaseButton = () => <button>HOMEBASE</button>;
+export const HomebaseButton = ({ children }) => <Button>{children}</Button>;
+// export const HomebaseButton = () => <button>HOMEBASE</button>;
